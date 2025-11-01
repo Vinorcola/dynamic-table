1301cccf8663abe100e63def047d2a7641bdd5e0
Vinorcola <vinorcola@gmail.com>
2025-11-01 18:36:17 +0100

    Fix Eslint error on lib directory

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import config from "@vinorcola/lint"
-import { defineConfig } from "eslint/config"
+import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
+    globalIgnores(["lib/*"]),
     {
         extends: [config],
         files: ["src/**/*.{js,jsx,ts,tsx}", "eslint.config.js"],
